418077de12cfd0c857226a85f27f1c7a0b9f341d
NickGhignatti <nick.ghignatti@gmail.com>
2025-11-11 12:09:05 +0100

    Remove unused imports

--- delivery/src/main/java/org/example/DeliveryService.java
+++ delivery/src/main/java/org/example/DeliveryService.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
